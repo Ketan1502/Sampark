04fc72de5c6b5c765ec9471ef0bf118b746aab72
Ketan1502 <ketanpoddar0215@gmail.com>
2021-05-20 10:39:23 +0530

    Login and Signup issue solved

--- Sampark/Team_Sampark/views.py
+++ Sampark/Team_Sampark/views.py
@@ -4,20 +4,32 @@ from django.contrib import messages
 
 # Create your views here.
 def home(request):
+    return render(request,'home.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def contact(request):
+    return render(request,'contact.html')
+
+def login(request):
     if request.method=='POST':
         em=request.POST.get('email')
         pass1=request.POST.get('pass')
         obj=userDetail.objects.all()
-        if obj.email == em:
-            if obj.password == pass1:
-                messages.success(request,"You are logged in succesfully!") 
-            else:
-                messages.success(request,"You entered a wrong password!") 
+        for row in obj:
+            if row.email== em:
+                if row.password == pass1:
+                    messages.success(request,"You are logged in succesfully!") 
+                    return render(request,'home.html')
+                else:
+                    messages.success(request,"You entered a wrong password!") 
+                    return render(request,'home.html')
+        messages.warning(request,"You entered a wrong email!") 
+    return render(request,'home.html')
+    
 
-           #messages.warning(request,"Your have entered wrong credentials")
-        else:
-            messages.warning(request,"You entered a wrong email!") 
-
+def signup(request):
     if request.method=='POST':
         name=request.POST.get('name')
         email=request.POST.get('email')
@@ -30,9 +42,4 @@ def home(request):
         else:
             messages.warning(request,"Your have entered a wrong password")
     return render(request,'home.html')
-
-def about(request):
-    return render(request,'about.html')
-
-def contact(request):
-    return render(request,'contact.html')
+    
